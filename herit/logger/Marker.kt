9cdb620bf0a5319872d1f5aa2156f40e106b91db
seongwon <developer.seongwon@gmail.com>
2026-02-15 15:04:08 +0900

    필요없는 디렉토리 제거

--- herit/logger/Marker.kt
+++
@@ -1,9 +0,0 @@
-package net.herit.logger
-
-import org.slf4j.MarkerFactory
-
-object Marker {
-    val Error = MarkerFactory.getMarker("ERROR");
-    val Out = MarkerFactory.getMarker("OUT");
-    val Call = MarkerFactory.getMarker("CALL");
-}
